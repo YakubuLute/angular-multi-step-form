355d856aab05c67c3eb17d5d5f934e59c89cf59c
Yakubu Lute <yakubulute@outlook.com>
2025-06-28 22:37:57 +0000

    Added a wrapper component

--- src/app/features/signup/components/signup-wrapper/signup-wrapper.component.ts
+++ src/app/features/signup/components/signup-wrapper/signup-wrapper.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { SignupDataService } from '../../../services/signup/signup.service';
 
 @Component({
   selector: 'app-signup-wrapper',
-  imports: [],
   templateUrl: './signup-wrapper.component.html',
-  styleUrl: './signup-wrapper.component.scss'
+  styleUrls: ['./signup-wrapper.component.scss']
 })
-export class SignupWrapperComponent {
+export class SignupWrapperComponent implements OnInit {
 
+  constructor(
+    private router: Router,
+    private signupDataService: SignupDataService
+  ) {}
+
+  ngOnInit(): void {
+    // Update current step when route changes
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.updateCurrentStepFromRoute(event.url);
+      });
+  }
+
+  private updateCurrentStepFromRoute(url: string): void {
+    let step = 1;
+
+    if (url.includes('select-plan')) {
+      step = 2;
+    } else if (url.includes('add-ons')) {
+      step = 3;
+    } else if (url.includes('summary')) {
+      step = 4;
+    }
+
+    this.signupDataService.updateCurrentStep(step);
+  }
 }
