5cce69b0f398ebf490b4d92feaa10ed653c66d40
Yakubu Lute <yakubulute@outlook.com>
2025-06-28 22:09:28 +0000

    Folder structuring

---
+++ src/app/shared/components/step-indicator/step-indicator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-indicator',
+  imports: [],
+  templateUrl: './step-indicator.component.html',
+  styleUrl: './step-indicator.component.scss'
+})
+export class StepIndicatorComponent {
+
+}
